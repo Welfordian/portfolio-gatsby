1903d2e4aaa815bf075d4504bc7f92c8fbe46fd6
Joshua Welford <josh@welford.me>
2021-07-24 02:08:22 -0400

    Adds Service Worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,3 +10,15 @@ import "./src/css/index.css"
 
 import wrapWithProvider from "./wrap-with-provider"
 export const wrapRootElement = wrapWithProvider
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+        `This application has been updated. ` +
+        `Reload to display the latest version?`
+    )
+    if (answer === true) {
+        window.location.reload()
+    }
+}
+
+export const registerServiceWorker = () => true
