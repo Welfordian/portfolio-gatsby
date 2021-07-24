d2652578c3c14d5a0bf6b3ece5f73341dda05e79
Joshua Welford <josh@welford.me>
2021-07-24 04:30:43 -0400

    Adds code splitting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import * as React from "react"
-import CaseStudies from "../components/Home/CastStudies";
-import PersonalProjects from "../components/Home/PersonalProjects";
-import Layout from "../components/Layout";
+import loadable from '@loadable/component'
+
+const Layout = loadable(() => import('../components/Layout'));
+const CaseStudies = loadable(() => import('../components/Home/CaseStudies'));
+const PersonalProjects = loadable(() => import('../components/Home/PersonalProjects'));
 
 const IndexPage = () => (
     <Layout>
