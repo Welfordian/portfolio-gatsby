fd79924975e44b940913655a74a62b4a04d10ec9
Joshua Welford <Josh@fish.cat>
2022-07-27 01:56:04 -0400

    Update service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
