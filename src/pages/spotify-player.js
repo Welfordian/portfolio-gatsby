40bf5c0a7e509c9ecdafec2727eb10a04145282c
Joshua Welford <Josh@fish.cat>
2022-07-27 01:24:51 -0400

    Fix spotify player

--- src/pages/spotify-player.js
+++ src/pages/spotify-player.js
@@ -40,7 +40,7 @@ class SpotifyPlayer extends React.Component {
                         <SpotifyAuth
                             redirectUri='https://welford.me/spotify-player/'
                             clientID='6d08c3a931c74f95813b4f181ef0ad50'
-                            scopes={[Scopes.userReadPrivate, 'user-read-email']}
+                            scopes={[Scopes.userReadPrivate, Scopes.userReadEmail, Scopes.streaming]}
                             title={"Connect to Spotify"}
                             logoClassName={"w-8 mr-8 fill-current text-white"}
                             btnClassName={"flex bg-black text-white items-center px-8 py-3"}
