70baca956fe436a60fd82ccf3bc2311b7d341c01
Joshua Welford <Josh@fish.cat>
2022-07-27 05:18:04 -0400

    Remove rounded corners for Safari

--- src/components/SpotifyPlayer/PlayerImage.js
+++ src/components/SpotifyPlayer/PlayerImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 class PlayerImage extends React.Component {
     render() {
         return (
-            <img className={`w-96`} src={this.props.playerState.track_window.current_track.album.images[0].url} />
+            <img className={`w-96 self-center`} src={this.props.playerState.track_window.current_track.album.images[0].url} />
         )
     }
 }
--- src/components/SpotifyPlayer/PlayerInfo.js
+++ src/components/SpotifyPlayer/PlayerInfo.js
@@ -3,8 +3,8 @@ import React from 'react';
 class PlayerInfo extends React.Component {
     render () {
         return (
-            <div className={`text-white`}>
-                <p>{this.props.playerState.track_window.current_track.name}</p>
+            <div className={`text-white max-w-full`}>
+                <p className={`break-words`}>{this.props.playerState.track_window.current_track.name}</p>
                 <p>{this.props.playerState.track_window.current_track.artists[0].name}</p>
             </div>
         )
