5f69a3319d90c99a8a5bb407f3ebb86dc1a03c5d
Joshua Welford <Josh@fish.cat>
2022-07-29 08:27:46 -0400

    Adds preconnect & dns-prefetch

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,3 +8,15 @@
 
 import wrapWithProvider from "./wrap-with-provider"
 export const wrapRootElement = wrapWithProvider
+import React from "react";
+
+export function onRenderBody(
+    { setHeadComponents }
+) {
+    setHeadComponents([
+        <link rel="preconnect" href="https://imagedelivery.net" />,
+        <link rel="dns-prefetch" href="https://imagedelivery.net" />,
+        <link rel="preconnect" href="https://api.welford.me" />,
+        <link rel="dns-prefetch" href="https://api.welford.me" />
+    ]);
+}
