ca4f7adc7cb1ef5903039487f312675b83b46c31
Joshua Welford <Josh@fish.cat>
2022-08-10 02:31:46 -0400

    Come on Cloudflare

--- src/components/WatchTogether/app.js
+++ src/components/WatchTogether/app.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        const socket = io(`${process.env.SOCKET_URL}`, {
+        const socket = io(`${process.env.SOCKET_URL ?? 'https://watch-together.welford.me'}`, {
             query: {
                 roomid: isBrowser ? window.location.pathname.replace('/watch-together/','') : null,
                 name: this.state.name
