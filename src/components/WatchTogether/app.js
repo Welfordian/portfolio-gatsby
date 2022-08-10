f9d128acd62b34efd8ebd6e2690111ef279987e9
Joshua Welford <Josh@fish.cat>
2022-08-10 02:43:21 -0400

    Oops

--- src/components/WatchTogether/app.js
+++ src/components/WatchTogether/app.js
@@ -16,7 +16,7 @@ class App extends React.Component {
         
         this.state = {
             modalOpen: true,
-            name: "Josh",
+            name: "",
             connected: false,
             socketId: null,
             socket: null,
