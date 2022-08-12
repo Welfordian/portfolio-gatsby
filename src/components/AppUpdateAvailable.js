c70b670b3f522cee91ed0b58af4c031a98eebac6
Joshua Welford <Josh@fish.cat>
2022-08-12 02:49:36 -0400

    Fix update alert

--- src/components/AppUpdateAvailable.js
+++ src/components/AppUpdateAvailable.js
@@ -15,7 +15,7 @@ class AppUpdateAvailable extends React.Component {
         if (! this.state.open) return (<div className={`absolute`}></div>);
         
         return (
-            <div className="fixed bottom-0 w-1/4 right-0 mr-4 p-4 mb-4 bg-opacity-90 bg-gray-700 shadow shadow-current shadow-md" role="alert">
+            <div className="fixed bottom-0 w-full md:w-2/4 lg:w-1/4 right-0 md:ml-0 md:mr-4 p-4 md:mb-4 bg-opacity-90 bg-gray-700 shadow shadow-current shadow-md" role="alert">
                 <div className="flex items-center">
                     <svg aria-hidden="true" className="w-5 h-5 mr-2 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
