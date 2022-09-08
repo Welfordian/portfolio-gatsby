a047f50d3a3ac29d9b21f7211e529db3f59e8a4f
Joshua Welford <Josh@fish.cat>
2022-09-08 14:41:32 -0400

    mobile fix

--- src/components/TemporaryOverlay.js
+++ src/components/TemporaryOverlay.js
@@ -6,7 +6,7 @@ class TemporaryOverlay extends React.Component {
     constructor(props) {
         super(props);
 
-        let isVisible = isBrowser() && localStorage.getItem('overlay-seen') === null ? true : false;
+        let isVisible = true; //isBrowser() && localStorage.getItem('overlay-seen') === null ? true : false;
 
         this.state = {
             visible: isVisible,
@@ -15,7 +15,7 @@ class TemporaryOverlay extends React.Component {
 
     componentDidMount() {
         setTimeout(() => {
-            this.setState({ visible: false })
+            //this.setState({ visible: false })
 
             localStorage.setItem('overlay-seen', true);
         }, 6500);
@@ -33,8 +33,8 @@ class TemporaryOverlay extends React.Component {
                 leaveTo="opacity-0"
             >
                 <div className={`fixed top-0 left-0 w-full h-screen bg-black text-white flex flex-col gap-12 items-center justify-center z-[60]`}>
-                    <p className={`text-6xl`}>Queen Elizabeth II</p>
-                    <p className={`text-5xl`}>1926 - 2022</p>
+                    <p className={`text-4xl md:text-6xl`}>Queen Elizabeth II</p>
+                    <p className={`text-3xl md:text-5xl`}>1926 - 2022</p>
                     <img src="/Vignette_Queen.jpg" />
                 </div>
             </Transition>
